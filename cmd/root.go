86112f5d4116bc2819754df98a079356f1295d2d
Unknown Application <unknown@airwallex.com>
2021-07-18 11:07:48 +0200

    feat: fixed Cobra command, simplified usage

--- cmd/root.go
+++ cmd/root.go
@@ -16,12 +16,9 @@ limitations under the License.
 package cmd
 
 import (
-	"bufio"
-	"fmt"
 	"os"
 	"strings"
 
-	"github.com/fatih/color"
 	"github.com/spf13/cobra"
 	"go.awx.im/challenges/rpn-calculator/pkg/calculator"
 )
@@ -31,20 +28,6 @@ var calc *calculator.Calculator
 
 // whether the calculator should run in interactive mode
 var interactive bool
-
-var blue = color.New(color.FgHiBlue).SprintfFunc()
-var white = color.New(color.FgHiWhite).SprintfFunc()
-var grey = color.New(color.FgWhite).SprintfFunc()
-var green = color.New(color.FgGreen).SprintfFunc()
-var red = color.New(color.FgRed).SprintfFunc()
-
-func printOutput(c *calculator.Calculator, i string, printInput bool) {
-	if err := c.Calculate(i); err != nil {
-		fmt.Println(red("x"), white(err.Error()))
-	} else {
-		fmt.Println(green("="), white(c.Stack().String()))
-	}
-}
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -57,30 +40,17 @@ var rootCmd = &cobra.Command{
 	// should implement the Operator interface which allows the calculator
 	// to iterate over a the stack and apply any operator methods to its
 	// aggregated result
-	PersistentPreRunE: func(cmd *cobra.Command, args []string) (err error) {
-		calc, err = calculator.NewDefaultCalculator()
+	PreRunE: func(cmd *cobra.Command, args []string) (err error) {
+		if calc, err = calculator.NewDefaultCalculator(); err != nil {
+			return
+		}
 		return
 	},
 	RunE: func(cmd *cobra.Command, args []string) (err error) {
 		if interactive {
-			reader := bufio.NewReader(os.Stdin)
-			for {
-				fmt.Print(blue("> "), grey(calc.Stack().String()), " ")
-				input, err := reader.ReadString('\n')
-				if err == nil {
-					if err := calc.Calculate(input); err != nil {
-						fmt.Println(red("x"), red(err.Error()))
-					}
-				} else {
-					fmt.Println(err)
-				}
-			}
-		} else {
-			input := strings.Join(args, " ")
-			fmt.Println(blue(">"), grey(input))
-			printOutput(calc, input, true)
+			return calculator.RunInteractiveCalculator(calc, os.Stdout, os.Stdin)
 		}
-		return
+		return calculator.RunCalculator(calc, os.Stdout, strings.Join(args, " "))
 	},
 }
 
